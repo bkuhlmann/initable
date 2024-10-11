aa11a54cffa7a3887d8f8a569f2440e3f27168d7
Brooke Kuhlmann <brooke@alchemists.io>
2024-10-11 14:53:02 -0600

    Added primary object API
    Necessary to provide the primary object API for this gem.

--- lib/initable.rb
+++ lib/initable.rb
@@ -5,4 +5,10 @@ require "initable/builder"
 # Main namespace.
 module Initable
   METHOD_SCOPES = %i[public protected private].freeze
+
+  def self.[](*) = Builder.new(*)
+
+  def self.protected(*) = Builder.new(*, scope: __method__)
+
+  def self.public(*) = Builder.new(*, scope: __method__)
 end
--- spec/lib/initable_spec.rb
+++ spec/lib/initable_spec.rb
@@ -3,5 +3,29 @@
 require "spec_helper"
 
 RSpec.describe Initable do
-  it "will be tested"
+  context "with private scope" do
+    subject(:initable) { Class.new.include(described_class[[:key, :one, 1]]).new }
+
+    it "prevents private access" do
+      expectation = proc { initable.one }
+      expect(&expectation).to raise_error(NoMethodError, /private method 'one'/)
+    end
+  end
+
+  context "with protected scope" do
+    subject(:initable) { Class.new.include(described_class.protected([:key, :one, 1])).new }
+
+    it "prevents private access" do
+      expectation = proc { initable.one }
+      expect(&expectation).to raise_error(NoMethodError, /protected method 'one'/)
+    end
+  end
+
+  context "with public scope" do
+    subject(:initable) { Class.new.include(described_class.public([:key, :one, 1])).new }
+
+    it "allows public access" do
+      expect(initable.one).to eq(1)
+    end
+  end
 end
